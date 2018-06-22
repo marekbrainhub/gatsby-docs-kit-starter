35468540cdf45de3a204157ba54b2723ae946f33
marekbrainhub <marek@brainhub.pl>
2018-06-22 09:08:40 +0200

    updating configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,8 @@ const pluginConfigFactory = require('@brainhubeu/gatsby-docs-kit/plugins');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brainhub template',
-    description: 'Default template for open-source libraries',
+    title: 'Hadron Docs',
+    description: 'Super duper Hadron website.',
     image: 'https://cdn-images-1.medium.com/max/1200/1*CLUFZFaXF6NG27NA3d_JkQ.jpeg',
     url: 'https://screencloud.dev.brainhub.pl/',
     type: 'article',
